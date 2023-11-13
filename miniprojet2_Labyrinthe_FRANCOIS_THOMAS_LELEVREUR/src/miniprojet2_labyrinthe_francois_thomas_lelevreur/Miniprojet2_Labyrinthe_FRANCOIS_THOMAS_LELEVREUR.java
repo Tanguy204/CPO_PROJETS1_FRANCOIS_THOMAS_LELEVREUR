332022e9cf7f5a47cf302ffa09586ca9701f99db
Tanguy <tanguy.francois@epfedu.fr>
2023-11-13 16:42:47 +0100

    Version 0

---
+++ miniprojet2_Labyrinthe_FRANCOIS_THOMAS_LELEVREUR/src/miniprojet2_labyrinthe_francois_thomas_lelevreur/Miniprojet2_Labyrinthe_FRANCOIS_THOMAS_LELEVREUR.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package miniprojet2_labyrinthe_francois_thomas_lelevreur;
+
+/**
+ *
+ * @author Tanguy
+ */
+public class Miniprojet2_Labyrinthe_FRANCOIS_THOMAS_LELEVREUR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
